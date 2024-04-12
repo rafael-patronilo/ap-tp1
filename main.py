ef4885a6e453ff999d2279837ca4f2032b5788f5
tomedias <td.dias@campus.fct.unl.pt>
2024-04-12 09:20:42 +0200

    Chnages

--- main.py
+++ main.py
@@ -110,6 +110,7 @@ layers_size = [256,128,64,32]
 layers_size = [x for x in layers_size for _ in range(1, 3)]
 for i in range(1,5):
     combinations = list(itertools.combinations(layers_size,i))
+    
     for x in combinations:
         if x not in already_tested:
             print(f'Using layers {list(x)}')
