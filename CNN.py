1afe490d2376563fb04244b893219293715e3303
rafael-patronilo <rafael.patronilo@hotmail.com>
2024-04-16 11:07:51 +0100

    Change generated VGGs to be smaller and be ordered by size

--- CNN.py
+++ CNN.py
@@ -86,19 +86,26 @@ def test_vgg(vgg_blocks, linear_layers):
 
 def test_vgg_architectures():
     print("-" * 196)
-    out_channels = [64, 128, 256, 512, 512]
-    all_conv_numbers = [x for x in itertools.product(range(1, 3), repeat=len(out_channels)) if sum(x) <= 8]
-    all_conv_numbers.extend([x for x in itertools.product(range(1, 3), repeat=len(out_channels)-1) if sum(x) <= 6])
-    linear_layer_sizes = [256, 1024, 4096]
-    best_f_score = None
-    all_linear_layers = zip(linear_layer_sizes, linear_layer_sizes)
-    for i, (conv_numbers, linear_layers) in enumerate(itertools.product(all_conv_numbers, all_linear_layers)):
-        vgg_blocks = tuple(zip(conv_numbers, out_channels))
+    out_channels = [16, 32, 64, 128, 256, 512, 512]
+    def neuron_count(architecture):
+        conv_numbers, linear_layers = architecture
+        return sum(linear_layers) + sum(out*count for out, count in zip(out_channels, conv_numbers))
+    all_conv_numbers = [x for x in itertools.product(range(4), repeat=len(out_channels)) 
+                        if sum(x) >= 1 and sum(1 for y in x if y > 0) >= 2]
+    linear_layer_sizes = [32, 64, 128, 256, 1024, 4096]
+    all_linear_layers = [x for layer_size in linear_layer_sizes for x in [[layer_size], [layer_size, layer_size]]]
+    all_linear_layers.append([])
+    architectures = list(itertools.product(all_conv_numbers, all_linear_layers))
+    architectures.sort(key=neuron_count)
+    for i, architecture in enumerate(architectures[:100]):
+        conv_numbers, linear_layers = architecture
+        vgg_blocks = zip(conv_numbers, out_channels)
         model, loss, accuracy, f_score = test_vgg(vgg_blocks, linear_layers)
         if best_f_score is None or f_score > best_f_score:
             best_f_score = f_score
             print(f"New best model found:{vgg_blocks} {linear_layers}")
             save_last_n(model, "best_vgg", 3)
+        print(f"Best f_score so far: {best_f_score}")
         print("-" * 196)
         
 
--- test.py
+++ test.py
@@ -26,14 +26,22 @@ import itertools
 # for i in range(10):
 #     save_last_n(input(), "model", 3)
 
+
 def test_vgg_architectures():
-    out_channels = [64, 128, 256, 512, 512]
-    all_conv_numbers = [x for x in itertools.product(range(1, 3), repeat=len(out_channels)) if sum(x) <= 8]
-    all_conv_numbers.extend([x for x in itertools.product(range(1, 3), repeat=len(out_channels)-1) if sum(x) <= 6])
-    linear_layer_sizes = [256, 1024, 4096]
-    all_linear_layers = zip(linear_layer_sizes, linear_layer_sizes)
-    for i, (conv_numbers, linear_layers) in enumerate(itertools.product(all_conv_numbers, all_linear_layers)):
+    out_channels = [16, 32, 64, 128, 256, 512, 512]
+    def neuron_count(architecture):
+        conv_numbers, linear_layers = architecture
+        return sum(linear_layers) + sum(out*count for out, count in zip(out_channels, conv_numbers))
+    all_conv_numbers = [x for x in itertools.product(range(4), repeat=len(out_channels)) 
+                        if sum(x) >= 1 and sum(1 for y in x if y > 0) >= 2]
+    linear_layer_sizes = [32, 64, 128, 256, 1024, 4096]
+    all_linear_layers = [x for layer_size in linear_layer_sizes for x in [[layer_size], [layer_size, layer_size]]]
+    all_linear_layers.append([])
+    architectures = list(itertools.product(all_conv_numbers, all_linear_layers))
+    architectures.sort(key=neuron_count)
+    for i, architecture in enumerate(architectures[:100]):
+        conv_numbers, linear_layers = architecture
         vgg_blocks = zip(conv_numbers, out_channels)
-        print(i, tuple(vgg_blocks), linear_layers)
+        print(i, neuron_count(architecture), tuple(block for block in vgg_blocks if block[0]>0), linear_layers)
 
 test_vgg_architectures()
