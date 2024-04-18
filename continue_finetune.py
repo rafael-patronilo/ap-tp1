1feeda2d2005002d4f1a7cd661c7d21e60bad8f3
rafael-patronilo <rafael.patronilo@hotmail.com>
2024-04-18 13:26:14 +0100

    fix model name extraction

--- continue_finetune.py
+++ continue_finetune.py
@@ -1,20 +1,21 @@
 import finetune
 import sys
 import torch
+import os
 
-filename = sys.argv[1]
+filepath = sys.argv[1]
 from_epoch = int(sys.argv[2])
 epochs = int(sys.argv[3])
 
 model_name = None
 
-parts = filename.split("_")
+parts = os.path.split(filepath)[1].split("_")
 
 if parts[1] == "best":
     model_name = "_".join(parts[2:-1])
 else:
     model_name = "_".join(parts[1:-1])
 
-model = torch.load(filename, map_location=finetune.device)
+model = torch.load(filepath, map_location=finetune.device)
 finetune.train_fine_tuning(model_name, model, 0.001, 
                            param_group=True, from_epoch=from_epoch, epochs=epochs)
