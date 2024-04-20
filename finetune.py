b56f2c79e929a05ec2226db64e2e44d6e52fc9ed
rafael-patronilo <rafael.patronilo@hotmail.com>
2024-04-20 15:06:33 +0100

    fix f1

--- finetune.py
+++ finetune.py
@@ -126,7 +126,7 @@ def evaluate(model, loss_fn, loader):
     with torch.no_grad():
         model.eval()
         test_loss, correct = 0, 0
-        f_score = MulticlassF1Score(device=device)
+        f_score = MulticlassF1Score(num_classes=18, average='macro', device=device)
 
         for X, y in loader:
             print(".", end="")
